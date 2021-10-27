ba14e110f83bd60d1c221e2069f2b8b32f204e86
LSH0809 <skyey94@naver.com>
2021-10-28 02:24:15 +0900

    refactor : Dto에서 User로 Validation 수정
    - PatchMapping에서의 유효성 검증으로 인해 Dto에서 Entity로 Validation 수정

--- src/main/java/org/prgrms/cream/domain/user/domain/User.java
+++ src/main/java/org/prgrms/cream/domain/user/domain/User.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -18,17 +21,23 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotBlank(message = "닉네임을 입력해 주세요.")
 	@Column(nullable = false, unique = true, length = 45)
 	private String nickname;
 
+	@NotBlank(message = "이메일을 입력해 주세요.")
+	@Email(message = "올바른 이메일 주소를 입력해 주세요.")
 	@Column(nullable = false, unique = true, length = 50)
 	private String email;
 
+	@NotBlank(message = "휴대폰 번호를 입력해 주세요.")
+	@Pattern(regexp = "(01[016789])(\\d{3,4})(\\d{4})", message = "올바른 휴대폰 번호를 입력해 주세요.")
 	@Column(nullable = false, unique = true, length = 45)
 	private String phone;
 
 	private String size;
 
+	@NotBlank(message = "주소를 입력해 주세요.")
 	@Column(nullable = false, length = 100)
 	private String address;
 
--- src/main/java/org/prgrms/cream/domain/user/dto/UserSignUpRequest.java
+++ src/main/java/org/prgrms/cream/domain/user/dto/UserSignUpRequest.java
@@ -9,20 +9,14 @@ import org.prgrms.cream.domain.user.domain.User;
 @Getter
 public class UserSignUpRequest {
 
-	@NotBlank(message = "닉네임을 입력해 주세요.")
 	private String nickname;
 
-	@NotBlank(message = "이메일을 입력해 주세요.")
-	@Email(message = "올바른 이메일 주소를 입력해 주세요.")
 	private String email;
 
-	@NotBlank(message = "휴대폰 번호를 입력해 주세요.")
-	@Pattern(regexp = "(01[016789])(\\d{3,4})(\\d{4})", message = "올바른 휴대폰 번호를 입력해 주세요.")
 	private String phone;
 
-	private int size;
+	private String size;
 
-	@NotBlank(message = "주소를 입력해 주세요.")
 	private String address;
 
 	public User toEntity() {
