1966f559a71fc61c60ea734021db4bb61ea55692
LSH0809 <skyey94@naver.com>
2021-10-27 16:08:40 +0900

    refactor : User 엔티티 @Column 옵션 추가

--- src/main/java/org/prgrms/cream/domain/user/domain/User.java
+++ src/main/java/org/prgrms/cream/domain/user/domain/User.java
@@ -1,5 +1,6 @@
 package org.prgrms.cream.domain.user.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,17 +19,22 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(nullable = false,unique = true,length = 45)
 	private String nickname;
 
+	@Column(nullable = false, unique = true, length = 50)
 	private String email;
 
+	@Column(nullable = false, unique = true,length = 45)
 	private String phone;
 
 	@Range(min = 220, max = 300)
 	private int size;
 
+	@Column(nullable = false, length = 100)
 	private String address;
 
+	@Column(nullable = false)
 	private boolean isDeleted = false;
 
 	protected User() {
