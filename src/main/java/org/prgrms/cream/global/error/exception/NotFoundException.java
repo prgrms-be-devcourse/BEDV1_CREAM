7d9ad3fd31a125f92ca790121794d55b77e6783c
LSH0809 <skyey94@naver.com>
2021-10-29 15:20:36 +0900

    refactor : NotFoundException 상속 구조 변경
     - NotFoundException이 BusinessException 을 상속 받을 수 있도록 변경
     - NotFoundExcetpion을 BusinessException의 하위 관계로 보는 것이 맞다고 생각하여 변경

--- src/main/java/org/prgrms/cream/global/error/exception/NotFoundException.java
+++ src/main/java/org/prgrms/cream/global/error/exception/NotFoundException.java
@@ -2,15 +2,9 @@ package org.prgrms.cream.global.error.exception;
 
 import org.prgrms.cream.global.error.ErrorCode;
 
-public class NotFoundException extends RuntimeException {
-
-	private final ErrorCode errorCode;
+public class NotFoundException extends BusinessException {
 
 	public NotFoundException(ErrorCode errorCode) {
-		this.errorCode = errorCode;
-	}
-
-	public ErrorCode getErrorCode() {
-		return errorCode;
+		super(errorCode);
 	}
 }
