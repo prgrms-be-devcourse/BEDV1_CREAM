ed1ff257c93cd5527a339ba6d42847937b95bc20
samkimuel <samkim2626@gmail.com>
2021-10-29 17:10:17 +0900

    style: 불필요한 어노테이션 제거

--- src/main/java/org/prgrms/cream/domain/deal/repository/BuyingRepository.java
+++ src/main/java/org/prgrms/cream/domain/deal/repository/BuyingRepository.java
@@ -2,9 +2,7 @@ package org.prgrms.cream.domain.deal.repository;
 
 import org.prgrms.cream.domain.deal.domain.BuyingBid;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BuyingRepository extends JpaRepository<BuyingBid, Long> {
 
 }
