3ae500311d4d6c9ab5f9a5655f9f80f05ceb23d5
LSH0809 <skyey94@naver.com>
2021-10-30 21:58:43 +0900

    refactor : 잘못된 패키지 삭제
    - 브랜치에 잘못된 패키지 삭제
    - deal 패키지 삭제

--- src/main/java/org/prgrms/cream/domain/deal/repository/SellingRepository.java
+++
@@ -1,8 +0,0 @@
-package org.prgrms.cream.domain.deal.repository;
-
-import org.prgrms.cream.domain.deal.domain.SellingBid;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SellingRepository extends JpaRepository<SellingBid, Long> {
-
-}
