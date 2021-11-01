2b9c407bcc32462040093841063d635b9a7aa8da
samkimuel <samkim2626@gmail.com>
2021-11-02 04:27:37 +0900

    프로젝트 설정 수정
    - build.gradle 수정
    - 배포 스크립트 수정

--- src/test/java/org/prgrms/cream/CreamApplicationTests.java
+++ src/test/java/org/prgrms/cream/CreamApplicationTests.java
@@ -1,13 +1,8 @@
 package org.prgrms.cream;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CreamApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
