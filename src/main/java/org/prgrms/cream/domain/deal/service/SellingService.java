d553342d09240b550dd1f2282e3094e60c654c38
LSH0809 <skyey94@naver.com>
2021-11-01 18:09:33 +0900

    refactor : 구매입찰 개수에 따른 조건문 변경
    - 구매입찰에 따른 조건문  if-else문으로 구분짓도록 변경

--- src/main/java/org/prgrms/cream/domain/deal/service/SellingService.java
+++ src/main/java/org/prgrms/cream/domain/deal/service/SellingService.java
@@ -1,4 +1,3 @@
-
 package org.prgrms.cream.domain.deal.service;
 
 import java.util.List;
@@ -16,16 +15,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class SellingService {
 
-	private final ProductService productService;
-	private final UserService userService;
-	private final BuyingService buyingService;
-	private final DealService dealService;
-
 	private static final int FIRST_BID = 0;
 	private static final int SECOND_BID = 1;
 	private static final int ZERO = 0;
 	private static final int ONLY_ONE_BID = 1;
 	private static final int ALL_BID = 2;
+	private final ProductService productService;
+	private final UserService userService;
+	private final BuyingService buyingService;
+	private final DealService dealService;
 
 	public SellingService(
 		ProductService productService,
@@ -58,9 +56,7 @@ public class SellingService {
 
 		if (buyingBids.size() == ONLY_ONE_BID) {
 			productOption.updateBuyBidPrice(ZERO);
-		}
-
-		if (buyingBids.size() == ALL_BID) {
+		} else if (buyingBids.size() == ALL_BID) {
 			BuyingBid secondBuyingBid = buyingBids.get(SECOND_BID);
 			productOption.updateBuyBidPrice(secondBuyingBid.getSuggestPrice());
 		}
