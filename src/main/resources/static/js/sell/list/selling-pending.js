74889d71071100fc7123274bfbe81e8093040bb6
LSH0809 <skyey94@naver.com>
2021-11-04 04:36:00 +0900

    feat : 판매 관련 화면(UI) 구현
    - 판매내역 조회(각 상태별) 화면 구현
    - 즉시 판매 / 판매 입찰에 대한 화면 구현

---
+++ src/main/resources/static/js/sell/list/selling-pending.js
@@ -0,0 +1,44 @@
+$(function () {
+
+  $("#send").click(function () {
+    let userId = $('#search-input').val();
+    let status = $('#status option:selected').val();
+    status = encodeURI(status);
+
+    if ((userId != localStorage.getItem('userId')) && (userId != '')) {
+      localStorage.setItem('userId', userId);
+    }
+
+    userId = localStorage.getItem('userId');
+    let url = '/users/' + userId + '/selling/pending';
+    url = status === '' ? url : url + '?status=' + status;
+    $.ajax({
+      url: url,
+      type: "get",
+      dataType: "json",
+      success: function (data) {
+        console.log(data);
+        let html = '';
+        $("#table tr:gt(0)").remove();
+        $.each(data.data, function (index, item) {
+          for (key in item) {
+            html += '<tr>';
+            html += '<td padding = 150px >' + '<img src=' + item[key].image
+                + ' width = "100" height = "100" >'
+                + '</td>';
+            html += '<td>' + item[key].name + '</td>';
+            html += '<td>' + item[key].size + '</td>';
+            html += '<td>' + item[key].price + '</td>';
+            html += '<td>' + item[key].dealDate + '</td>';
+            html += '</tr>';
+          }
+        })
+        $("#dynamicTbody").empty();
+        $("#dynamicTbody").append(html);
+      },
+      error: function (err) {
+        alert("ajax 에러 발생");
+      }
+    });
+  })
+})
