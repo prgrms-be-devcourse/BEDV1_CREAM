52a253062a1b00711f3995fd2e6700bb384d97ff
LSH0809 <skyey94@naver.com>
2021-11-03 15:35:18 +0900

    refactor : 거래중/ 거래완료  응답객체반환 메소드 구조 변경
    - stream을 활용하여 불필요한 메소드 삭제
    - private 메소드로 리스트 반환하지 않고 엔티티 내부 메소드로 전환되도록 수정

--- src/main/java/org/prgrms/cream/domain/deal/service/DealService.java
+++ src/main/java/org/prgrms/cream/domain/deal/service/DealService.java
@@ -1,7 +1,5 @@
 package org.prgrms.cream.domain.deal.service;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.prgrms.cream.domain.deal.domain.BuyingBid;
 import org.prgrms.cream.domain.deal.domain.Deal;
 import org.prgrms.cream.domain.deal.dto.DealHistoryResponse;
@@ -9,7 +7,6 @@ import org.prgrms.cream.domain.deal.model.DealStatus;
 import org.prgrms.cream.domain.deal.repository.DealRepository;
 import org.prgrms.cream.domain.product.domain.Product;
 import org.prgrms.cream.domain.user.domain.User;
-import org.prgrms.cream.domain.user.dto.UserDealResponse;
 import org.prgrms.cream.domain.user.service.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -48,12 +45,15 @@ public class DealService {
 	public DealHistoryResponse getFinishedDealHistory(
 		Long id
 	) {
-		List<UserDealResponse> userDealResponses = new ArrayList<>();
-		List<Deal> deals = dealRepository.findAllBySellerAndIsFinishedTrue(
-			userService.findActiveUser(id)
+		return new DealHistoryResponse(
+			dealRepository
+				.findAllBySellerAndIsFinishedTrue(
+					userService.findActiveUser(id)
+				)
+				.stream()
+				.map(Deal::toUserDealResponse)
+				.toList()
 		);
-
-		return new DealHistoryResponse(addToDealResponse(userDealResponses, deals));
 	}
 
 	@Transactional(readOnly = true)
@@ -61,25 +61,31 @@ public class DealService {
 		Long id,
 		String status
 	) {
-		List<UserDealResponse> userDealResponses = new ArrayList<>();
-		List<Deal> deals = dealRepository.findAllBySellerAndSellingStatusAndIsFinishedTrue(
-			userService.findActiveUser(id),
-			status
+		return new DealHistoryResponse(
+			dealRepository
+				.findAllBySellerAndSellingStatusAndIsFinishedTrue(
+					userService.findActiveUser(id),
+					status
+				)
+				.stream()
+				.map(Deal::toUserDealResponse)
+				.toList()
 		);
-
-		return new DealHistoryResponse(addToDealResponse(userDealResponses, deals));
 	}
 
 	@Transactional(readOnly = true)
 	public DealHistoryResponse getPendingDealHistory(
 		Long id
 	) {
-		List<UserDealResponse> userDealResponses = new ArrayList<>();
-		List<Deal> deals = dealRepository.findAllBySellerAndIsFinishedFalse(
-			userService.findActiveUser(id)
+		return new DealHistoryResponse(
+			dealRepository
+				.findAllBySellerAndIsFinishedFalse(
+					userService.findActiveUser(id)
+				)
+				.stream()
+				.map(Deal::toUserDealResponse)
+				.toList()
 		);
-
-		return new DealHistoryResponse(addToDealResponse(userDealResponses, deals));
 	}
 
 	@Transactional(readOnly = true)
@@ -87,23 +93,16 @@ public class DealService {
 		Long id,
 		String status
 	) {
-		List<UserDealResponse> userDealResponses = new ArrayList<>();
-		List<Deal> deals = dealRepository.findAllBySellerAndSellingStatusAndIsFinishedFalse(
-			userService.findActiveUser(id),
-			status
+		return new DealHistoryResponse(
+			dealRepository
+				.findAllBySellerAndSellingStatusAndIsFinishedFalse(
+					userService.findActiveUser(id),
+					status
+				)
+				.stream()
+				.map(Deal::toUserDealResponse)
+				.toList()
 		);
-
-		return new DealHistoryResponse(addToDealResponse(userDealResponses, deals));
-	}
-
-	private List<UserDealResponse> addToDealResponse(
-		List<UserDealResponse> userDealResponses,
-		List<Deal> deals
-	) {
-		for (Deal deal : deals) {
-			userDealResponses.add(deal.toUserDealResponse());
-		}
-		return userDealResponses;
 	}
 
 	public Deal createDeal(Deal deal) {
