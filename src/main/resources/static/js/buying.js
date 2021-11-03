9c3aff7628f41931a822c82f8dc6becfe70b5692
samkimuel <samkim2626@gmail.com>
2021-11-04 02:10:26 +0900

    feat: 구매 관련 화면(UI) 구현
    - 구매 입찰, 즉시 구매 페이지 구현
    - 구매 내역(구매 입찰, 진행 중, 종료) 페이지 구현

---
+++ src/main/resources/static/js/buying.js
@@ -0,0 +1,89 @@
+$('#btn-product').on('click', getProduct);
+
+function getProduct() {
+  let productId = $("#productId").val();
+  let size = $("#size").val();
+
+  $.ajax({
+    type: 'GET',
+    url: '/products/' + productId + '?size=' + size
+  }).done(function (response) {
+    console.log(response)
+    $('#product-image').attr('src', response.data.image);
+    $('#product-englishName').text(response.data.englishName);
+    $('#product-koreanName').text(response.data.koreanName);
+
+    $.each(response.data.options, (idx, product) => {
+      if (product.size === size) {
+        $('#product-size').text(product.size);
+        $('#product-straightBuyPrice').text(
+            '즉시 구매가 : ' + product.straightBuyPrice);
+        $('#product-straightSellPrice').text(
+            '즉시 판매가 : ' + product.straightSellPrice);
+      }
+    });
+  }).fail(function (error) {
+    console.log(error)
+    alert(error);
+  })
+}
+
+$('#btn-buyingbid').on('click', buying);
+
+function buying() {
+  let productId = $('#productId').val();
+  let userId = $('#userId').val();
+  let price = $('#price').val();
+  let deadline = $('#deadline').val();
+  let size = $('#size').val();
+
+  let sendData = {
+    "price": price,
+    "deadline": deadline,
+    "userId": userId
+  }
+
+  $.ajax({
+    type: 'PUT',
+    url: '/buying/' + productId + '?size=' + size,
+    data: JSON.stringify(sendData),
+    dataType: 'json',
+    contentType: 'application/json; charset=utf-8',
+  }).done(function (response) {
+    $("#result-price").text('구매 희망가 : ' + response.data.price);
+    $("#result-deadline").text('입찰 마감 기한 : ' + response.data.deadline + '일');
+    $("#result-expiredDate").text(response.data.expiredDate + '까지');
+  }).fail(function (error) {
+    console.log(error);
+    console.log(error.responseJSON)
+    alert(error);
+  })
+}
+
+$('#btn-straightBuy').on('click', straightBuy);
+
+function straightBuy() {
+  let productId = $("#productId").val();
+  let size = $("#size").val();
+  let userId = $("#userId").val();
+
+  let sendData = {
+    "userId": userId
+  }
+
+  $.ajax({
+    type: 'POST',
+    url: '/buying/' + productId + '?size=' + size,
+    data: JSON.stringify(sendData),
+    dataType: 'json',
+    contentType: 'application/json; charset=utf-8',
+  }).done(function (response) {
+    $('straight-dealId').text('거래 체결 ID : ' + response.data.dealId);
+    $('#straight-productName').text('거래 상품 : ' + response.data.productName);
+    $('#straight-size').text('사이즈 : ' + response.data.size);
+    $('#straight-price').text('체결 거래가 : ' + response.data.price);
+    $('#straight-createdDate').text('체결일자 : ' + response.data.createdDate);
+  }).fail(function (error) {
+    alert(error);
+  })
+}
