0f08ca51647b5f46788f12aa4f5a4eae6fa0d48e
김부희 <kimbuhui@naver.com>
2021-11-05 03:33:54 +0900

    feat : 검색 기능 추가

--- src/main/resources/static/js/product/all-product.js
+++ src/main/resources/static/js/product/all-product.js
@@ -22,3 +22,32 @@ $(function () {
     }
   });
 });
+
+function getFilter() {
+  let keyword = $('#keyword').val();
+  $.ajax({
+    type: "GET",
+    url: "/products/search",
+    data: {
+      keyword: keyword,
+    },
+    success: (response) => {
+      $('#productList tbody').empty();
+      $.each(response.data, (idx, product) => {
+        let productRow = '<tr style="align-items: center; cursor:pointer;" '
+            + 'onClick="location.href=\'/product/one-product.html?'
+            + product.id + '\'">'
+            + '<td class=\"td_image\"><img src="' + product.image
+            + '"style="width:100px; height:100px;"/></td>'
+            + '<td class=\"td_brand\">' + product.brand + '</td>'
+            + '<td class=\"td_name\">' + product.englishName + '</td>'
+            + '<td class=\"td_name\">' + product.koreanName + '</td>'
+            + '<td class=\"td_buy_price\">' + product.straightBuyPrice + '</td>'
+            + '<td class=\"td_sell_price\">' + product.straightSellPrice
+            + '</td>'
+            + '</tr>';
+        $('#productList tbody').append(productRow);
+      });
+    }
+  });
+}
