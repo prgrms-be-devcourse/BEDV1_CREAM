defb8bef756b274ad7ec57ebb6af38503829acdb
LSH0809 <skyey94@naver.com>
2021-11-05 04:47:47 +0900

    refactor : @ApiOperation 삭제
    - 불필요한 @ApiOperation 삭제

--- src/main/java/org/prgrms/cream/domain/user/controller/UserController.java
+++ src/main/java/org/prgrms/cream/domain/user/controller/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@ApiOperation(value = "회원 컨트롤러")
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
--- src/main/java/org/prgrms/cream/domain/user/controller/UserShoppingInfoController.java
+++ src/main/java/org/prgrms/cream/domain/user/controller/UserShoppingInfoController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@ApiOperation(value = "쇼핑정보 및 관리 컨트롤러")
 @RestController
 @RequestMapping("/users/{userId}")
 public class UserShoppingInfoController {
