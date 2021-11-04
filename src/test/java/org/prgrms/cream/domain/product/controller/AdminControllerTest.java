c4f1641823480d85374e966fdcab88c891b3068c
samkimuel <samkim2626@gmail.com>
2021-11-04 21:42:15 +0900

    chore: gitignore 수정, profile 설정

--- src/test/java/org/prgrms/cream/domain/product/controller/AdminControllerTest.java
+++ src/test/java/org/prgrms/cream/domain/product/controller/AdminControllerTest.java
@@ -3,7 +3,6 @@ package org.prgrms.cream.domain.product.controller;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import javax.transaction.Transactional;
@@ -14,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -40,38 +38,6 @@ class AdminControllerTest {
 
 	@Autowired
 	private ObjectMapper objectMapper;
-
-	@DisplayName("상품 등록 테스트")
-	@Test
-	void registerProductTest() throws Exception {
-		ProductTestRequest productTestRequest = new ProductTestRequest(
-			BRAND,
-			ENGLISH_NAME,
-			KOREAN_NAME,
-			MODEL_NUMBER,
-			COLOR,
-			RELEASE_PRICE,
-			RELEASE_DATE,
-			SIZES
-		);
-
-		byte[] file = "image" .getBytes(StandardCharsets.UTF_8);
-		MockMultipartFile filePart = new MockMultipartFile(
-			"file", "test", null, file);
-		byte[] json = objectMapper
-			.writeValueAsString(productTestRequest)
-			.getBytes(StandardCharsets.UTF_8);
-		MockMultipartFile jsonPart = new MockMultipartFile(
-			"request", "request", "application/json", json);
-
-		this.mockMvc
-			.perform(MockMvcRequestBuilders
-						 .multipart("/admin/products")
-						 .file(filePart)
-						 .file(jsonPart))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.data").isNumber());
-	}
 
 	@DisplayName("상품 수정 테스트")
 	@Test
