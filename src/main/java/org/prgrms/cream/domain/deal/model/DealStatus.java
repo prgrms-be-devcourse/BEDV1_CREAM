f656bb2183f94f32c2abc9f7ae2b796eabade39c
samkimuel <samkim2626@gmail.com>
2021-11-04 21:10:45 +0900

    fix: 공통 수정사항 반영

--- src/main/java/org/prgrms/cream/domain/deal/model/DealStatus.java
+++ src/main/java/org/prgrms/cream/domain/deal/model/DealStatus.java
@@ -9,8 +9,11 @@ public enum DealStatus {
 	BID_CANCELLED("입찰 취소"),
 	BID_COMPLETED("입찰 완료"),
 	UNDER_INSPECTION("검수 중"),
+	INSPECTION_PASSED("검수합격"),
+	IN_TRANSIT("배송중"),
+	HOLD("보류"),
 	SHIP_COMPLETED("배송완료"),
-	PAYMENT_COMPLETED("결제 완료"),
+	PAYMENT_COMPLETED("정산완료"),
 	;
 
 	private final String status;
